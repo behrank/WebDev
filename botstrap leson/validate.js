11df792f29af0558e207b23eefea7390ef1a68eb
Behran Kankul <behrank@gmail.com>
2019-11-10 14:48:04 +0300

    update

--- botstrap leson/validate.js
+++ botstrap leson/validate.js
@@ -1,15 +1,32 @@
 const mail = document.getElementById('posta')
 const pass = document.getElementById('sifre')
 function myFunction() {
+    //bunları buttonda sadece validation yapıldığı için koydum. Statusu hata varsa göstersin diye.
+    document.getElementById("alert-modal").classList.remove("alert-success");
+    document.getElementById("alert-modal").classList.add("alert-danger");
+    //
+
+    document.getElementById("alert-modal").classList.add("hide");
 
     if (mail.value == '' && pass.value == '') {
-        alert("email ve sifre alanı boş")
+        updateAlertModel("email ve sifre alanı boş");
     } else if (pass.value == '' || pass.value == null) {
-
-        alert("şifre alanı boş")
+        updateAlertModel("şifre alanı boş");
     } else if (mail.value == '' || mail.value == null) {
-
-        alert("email alanı boş")
+        updateAlertModel("email alanı boş");
+    } else {
+        //Success
+        setLoginSuccess()
     }
+}
+function updateAlertModel(text) {
+    document.getElementById("alert-modal").classList.remove("hide");
+    document.getElementById("alert-modal").innerHTML = text;
+}
+function setLoginSuccess() {
+    document.getElementById("alert-modal").classList.remove("alert-danger");
+    document.getElementById("alert-modal").classList.remove("hide");
+    document.getElementById("alert-modal").classList.add("alert-success");
+    document.getElementById("alert-modal").innerHTML = "Herşey başarılı.";
 }
 
